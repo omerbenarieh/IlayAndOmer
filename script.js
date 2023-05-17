4f77a2e96e56add9e92cd0808bbf86c81d844100
omerbenarieh <omerrbenarieh@gmail.com>
2023-05-18 01:10:16 +0300

    style

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 /*
+  Creators:
   Omer Ben Arieh 207229634
   Ilay Cohen
 */
 
-const taskInput = document.getElementById('task-input');
-const addBtn = document.getElementById('add-btn');
-const taskListContainer = document.getElementById('task-list');
-const deleteMarkedBtn = document.getElementById('delete-marked-btn');
-const deleteAllBtn = document.getElementById('delete-all-btn');
-const markAsCompleteBtn = document.getElementById('mark-as-complete');
+//-------------------------------------------------\\
+// Selecting elements
+//-------------------------------------------------\\
+const taskInput = document.getElementById("task-input");
+const addBtn = document.getElementById("add-btn");
+const taskListContainer = document.getElementById("task-list");
+const deleteMarkedBtn = document.getElementById("delete-marked-btn");
+const deleteAllBtn = document.getElementById("delete-all-btn");
+const markAsCompleteBtn = document.getElementById("mark-as-complete");
 const completedTasksContainer = document.getElementById(
-  'completed-tasked-list'
+  "completed-tasked-list"
 );
 
 let toDoTasks = [];
 let completedTasks = [];
 
+//-------------------------------------------------\\
 // Call Backs
-const addNewTaskClick = e => {
+//-------------------------------------------------\\
+const addNewTaskClick = (e) => {
   // Check if task is valid
   const task = taskInput.value.trim();
   const taskLength = task.length;
   if (taskLength > 0 && taskLength < 30) {
     createTask(task);
-  } else alert('Please Insert a valid task ⚠️');
-  taskInput.value = '';
+  } else alert("Please Insert a valid task ⚠️");
+  taskInput.value = "";
 };
 
-const addNewTaskPress = e => {
+const addNewTaskPress = (e) => {
   const keyCode = e.keyCode;
   if (keyCode === 13) {
     const task = taskInput.value;
@@ -35,24 +41,24 @@ const addNewTaskPress = e => {
 
     if (taskLength > 0 && taskLength < 30) {
       createTask(task);
-    } else alert('Please Insert a valid task ⚠️');
+    } else alert("Please Insert a valid task ⚠️");
   }
 };
 
-taskListContainer.addEventListener('click', e => {
+taskListContainer.addEventListener("click", (e) => {
   const index = toDoTasks.indexOf(e.target.parentNode.parentNode);
   const action = e.target.textContent;
-  if (action === '❌') deleteTask(index);
-  if (action === '⬆️') upTask(index);
-  if (action === '⬇️') downTask(index);
+  if (action === "❌") deleteTask(index);
+  if (action === "⬆️") upTask(index);
+  if (action === "⬇️") downTask(index);
 });
 
-const deleteTask = index => {
+const deleteTask = (index) => {
   toDoTasks.splice(index, 1);
   render();
 };
 
-const upTask = index => {
+const upTask = (index) => {
   if (toDoTasks.length > 1) {
     if (index > 0) {
       [toDoTasks[index], toDoTasks[index - 1]] = [
@@ -69,7 +75,7 @@ const upTask = index => {
   render();
 };
 
-const downTask = index => {
+const downTask = (index) => {
   if (toDoTasks.length > 1) {
     if (index < toDoTasks.length - 1) {
       [toDoTasks[index], toDoTasks[index + 1]] = [
@@ -83,28 +89,28 @@ const downTask = index => {
   }
 };
 
-const deleteAll = e => {
+const deleteAll = (e) => {
   toDoTasks.splice(0);
   render();
 };
 
-const deleteMarked = e => {
-  toDoTasks = toDoTasks.filter(node => !node.children[0].checked);
+const deleteMarked = (e) => {
+  toDoTasks = toDoTasks.filter((node) => !node.children[0].checked);
   render();
 };
 
-const addFinishedTask = e => {
-  toDoTasks.forEach(task => {
+const addFinishedTask = (e) => {
+  toDoTasks.forEach((task) => {
     if (task.children[0].checked) completedTasks.push(task);
   });
-  toDoTasks = toDoTasks.filter(finished => !finished.children[0].checked);
+  toDoTasks = toDoTasks.filter((finished) => !finished.children[0].checked);
   editFinishedTasks();
   render();
 };
 
-// helper function
+// Helper fucntion \\
 const editFinishedTasks = () => {
-  // Defining Date.
+  // Defining Date \\
   const today = new Date();
   const day = today.getDate();
   const month = today.getMonth() + 1;
@@ -123,48 +129,52 @@ const editFinishedTasks = () => {
   });
 };
 
-const createTask = task => {
+const createTask = (task) => {
   const htmlString = `
     <input type="checkbox" class="checkbox">
     <p>${task}</p>
     <div>
-      <button class="btns">⬆️</button>
-      <button class="btns">⬇️</button>
-      <button class="btns">❌</button>
+      <button class="btns btn-direction">⬆️</button>
+      <button class="btns btn-direction">⬇️</button>
+      <button class="btns btn-delete">❌</button>
     </div>`;
 
-  const newTask = document.createElement('li', {
-    className: 'task',
-  });
+  const newTask = document.createElement("li");
+  newTask.className = "task";
 
   newTask.innerHTML = htmlString;
   toDoTasks.push(newTask);
   render();
 };
 
+//-------------------------------------------------\\
+// Render Function
+//-------------------------------------------------\\
 const render = () => {
-  taskInput.value = '';
-  taskListContainer.innerHTML = '';
-  completedTasksContainer.innerHTML = '';
+  taskInput.value = "";
+  taskListContainer.innerHTML = "";
+  completedTasksContainer.innerHTML = "";
 
-  // Todo Tasks
+  // Todo Tasks \\
   if (toDoTasks.length > 0) {
-    toDoTasks.forEach(node => {
-      taskListContainer.insertAdjacentElement('beforeend', node);
+    toDoTasks.forEach((node) => {
+      taskListContainer.insertAdjacentElement("beforeend", node);
     });
   }
 
-  // Completed Tasks
+  // Completed Tasks \\
   if (completedTasks.length > 0) {
-    completedTasks.forEach(node => {
-      completedTasksContainer.insertAdjacentElement('beforeend', node);
+    completedTasks.forEach((node) => {
+      completedTasksContainer.insertAdjacentElement("beforeend", node);
     });
   }
 };
 
-// Listeners
-addBtn.addEventListener('click', addNewTaskClick);
-taskInput.addEventListener('keydown', addNewTaskPress);
-deleteAllBtn.addEventListener('click', deleteAll);
-deleteMarkedBtn.addEventListener('click', deleteMarked);
-markAsCompleteBtn.addEventListener('click', addFinishedTask);
+//-------------------------------------------------\\
+// Event Listeners
+//-------------------------------------------------\\
+addBtn.addEventListener("click", addNewTaskClick);
+taskInput.addEventListener("keydown", addNewTaskPress);
+deleteAllBtn.addEventListener("click", deleteAll);
+deleteMarkedBtn.addEventListener("click", deleteMarked);
+markAsCompleteBtn.addEventListener("click", addFinishedTask);
